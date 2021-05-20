321c0f354a8d17ce9c1a0a15e541dbb2444701d6
yoyosource <yoyosource@nidido.de>
2021-05-20 10:22:03 +0200

    Update standard.scanrule

--- src/main/java/de/yoyosource/Skandieren.java
+++ src/main/java/de/yoyosource/Skandieren.java
@@ -1,16 +1,34 @@
 package de.yoyosource;
 
+import yapion.exceptions.parser.YAPIONParserException;
+import yapion.hierarchy.output.FileGZIPOutput;
+import yapion.hierarchy.output.LengthOutput;
 import yapion.hierarchy.types.YAPIONObject;
 import yapion.parser.YAPIONParser;
 
 import java.io.File;
-import java.io.IOException;
 
 public class Skandieren {
 
-    public static void main(String[] args) throws IOException {
-        YAPIONObject yapionObject = YAPIONParser.parse(new File("/Users/jojo/IdeaProjects/Skandieren2.0/src/main/resources/standard.scanrule"));
-        System.out.println(yapionObject);
+    public static void main(String[] args) throws Exception {
+        File toRead = new File("/Users/jojo/IdeaProjects/Skandieren2.0/src/main/resources/standard.scanrule");
+        long modified = 0;
+        while (true) {
+            if (toRead.lastModified() <= modified + 100) {
+                Thread.sleep(100);
+                continue;
+            }
+            modified = toRead.lastModified();
+            try {
+                YAPIONObject yapionObject = YAPIONParser.parse(new File("/Users/jojo/IdeaProjects/Skandieren2.0/src/main/resources/standard.scanrule"));
+                System.out.println(yapionObject);
+                File file = new File("/Users/jojo/IdeaProjects/Skandieren2.0/src/main/resources/standard.scanrule.gz");
+                yapionObject.toYAPION(new FileGZIPOutput(file)).close();
+                System.out.println(yapionObject.toYAPION(new LengthOutput()).getPrettifiedLength() + " " + yapionObject.toYAPION(new LengthOutput()).getLength() + " " + file.length());
+            } catch (YAPIONParserException e) {
+                System.out.println("INVALID INPUT");
+            }
+        }
     }
 
 }
