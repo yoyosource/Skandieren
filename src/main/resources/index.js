c609de0e67aff50062cccda08fb95f00b6beb1ff
yoyosource <yoyosource@nidido.de>
2021-05-31 12:00:48 +0200

    Add index.js.skandieren

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -6,3 +6,20 @@ function getInputValue(event) {
     let inputValue = document.getElementsByTagName("input")[0].value;
     alert(inputValue);
 }
+
+function skandieren(input) {
+    if (!(input typeOf String)) {
+        return
+    }
+    async () => {
+        const response = await fetch('<API>', {
+            method: 'POST',
+            body: myBody, // string or object
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const myJson = await response.json(); //extract JSON from the http response
+        // do something with myJson
+    }
+}
