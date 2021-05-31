49a6d35a48183a03df53286419c32273f90afd01
yoyosource <yoyosource@nidido.de>
2021-05-31 11:57:24 +0200

    Fix index.js

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -2,6 +2,7 @@ function getInputValue(event) {
     if (event.keyCode != 13) {
         return;
     }
+    event.preventDefault();
     let inputValue = document.getElementsByTagName("input")[0].value;
     alert(inputValue);
 }
