740a845f47ec9cdea79555db450cc031a88841e7
yoyosource <yoyosource@nidido.de>
2021-06-01 10:38:06 +0200

    Update index.css Update index.html Update index.js

--- src/main/resources/index.js
+++ src/main/resources/index.js
@@ -3,16 +3,39 @@ function getInputValue(event) {
         return;
     }
     event.preventDefault();
-    let inputValue = document.getElementsByTagName("input")[0].value;
-    alert(inputValue);
+    let inputValue = document.getElementById("input").value;
+    // alert(inputValue);
+
+    let div = document.getElementById("output");
+    div.innerHTML = '';
+    let result = skandieren(inputValue);
+
+    for (var index in result?.results) {
+        var oneElement = result?.results?.[index];
+        if (!(oneElement?.text)) {
+            var paragraph = document.createElement('br');
+            div.appendChild(paragraph);
+            continue;
+        }
+        console.log(oneElement)
+        if (oneElement?.percent) {
+            var percentDiv = document.createElement('div');
+            percentDiv.setAttribute('class', 'percent');
+            percentDiv.innerHTML = oneElement?.percent + '%';
+            div.appendChild(percentDiv);
+        }
+        var textDiv = document.createElement('div');
+        textDiv.setAttribute('class', 'text');
+        textDiv.innerHTML = oneElement?.text;
+        div.appendChild(textDiv);
+    }
 }
 
 function skandieren(input) {
     if (!(typeof(input) === "string")) {
         return;
     }
-    console.log("Hello World 2")
-    async () => {
+    /*async () => {
         const response = await fetch('<API>', {
             method: 'POST',
             body: myBody, // string or object
@@ -22,5 +45,27 @@ function skandieren(input) {
         });
         const myJson = await response.json(); //extract JSON from the http response
         // do something with myJson
+    }*/
+    let results = [];
+    let result = {
+        "results": results
+    };
+    for (var i = 0; i < Math.floor((Math.random() * 40) + 1); i++) {
+        let number = Math.floor(Math.random() * 1000) / 10.0;
+        let random = Math.random();
+        if (random > 0.7) {
+            results.push({
+                "text": input,
+                "percent": number
+            });
+        } else if (random > 0.1) {
+            results.push({
+                "text": input,
+            });
+        } else {
+            results.push({});
+        }
     }
+    console.log(result);
+    return result;
 }
